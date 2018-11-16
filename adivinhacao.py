8943176c760f853ad5940fe4d3c8e2ab8a7044df
Danilo Domingos <danilo.domingos02@gmail.com>
2018-11-16 09:59:01 -0200

    [AULA 4][01] - Adicionando while ao jogo.

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,21 +3,28 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("**********************************")
 
 numero_secreto = 42
+total_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite o seu numero: ")
-print("Você digitou ", chute_str)
-chute = int(chute_str)
+while(rodada <= total_tentativas):
+    print("Tentativa ", rodada, "de ", total_tentativas)
+    chute_str = input("Digite o seu numero: ")
+    print("Você digitou ", chute_str)
+    chute = int(chute_str)
 
-acertou = (numero_secreto == chute)
-maior = (chute > numero_secreto)
-menor = (chute < numero_secreto)
+    acertou = (numero_secreto == chute)
+    maior = (chute > numero_secreto)
+    menor = (chute < numero_secreto)
 
-if(acertou):
-    print("Você acertou!")
-else:
-    if(maior):
-        print("Você errou! O seu chute foi maior do que o número secreto.")
-    elif(menor):
-        print("Você errou! O seu chute foi menor do que o número secreto.")
+    if(acertou):
+        print("Você acertou!")
+        break;
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior do que o número secreto.")
+        elif(menor):
+            print("Você errou! O seu chute foi menor do que o número secreto.")
+    
+    rodada += 1
 
 print("Fim do jogo.")
