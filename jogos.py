2ae7148a27fad9564646ffcecea1441c0f84e811
Danilo Domingos <danilo.domingos02@gmail.com>
2018-11-16 16:26:38 -0200

    [AULA 8][01] - Fazendo import nos arquivos.

---
+++ jogos.py
@@ -0,0 +1,9 @@
+import adivinhacao
+import forca
+
+print("**********************************")
+print("Escolha o seu jogo!")
+print("**********************************")
+
+print("(1) Forca (2) Advinhação")
+jogo = int(input("Qual jogo? "))
