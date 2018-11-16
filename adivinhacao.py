5d12a70a12030eaf42b25a0b98dcdcea2cf2d5f3
Danilo Domingos <danilo.domingos02@gmail.com>
2018-11-16 10:13:38 -0200

    [AULA 4][04] - Interpolação de Strings.

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,11 +3,11 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("**********************************")
 
 numero_secreto = 42
-total_tentativas = 3
+total_de_tentativas = 3
 rodada = 1
 
-while(rodada <= total_tentativas):
-    print("Tentativa ", rodada, "de ", total_tentativas)
+while(rodada <= total_de_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
     chute_str = input("Digite o seu numero: ")
     print("Você digitou ", chute_str)
     chute = int(chute_str)
@@ -18,7 +18,7 @@ while(rodada <= total_tentativas):
 
     if(acertou):
         print("Você acertou!")
-        break;
+        break
     else:
         if(maior):
             print("Você errou! O seu chute foi maior do que o número secreto.")
