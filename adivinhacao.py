a4e89549ee6634eb6a031202ffadf3587babb872
Danilo Domingos <danilo.domingos02@gmail.com>
2018-11-16 11:21:28 -0200

    [AULA 5][04] - Usando o break e o continue

--- adivinhacao.py
+++ adivinhacao.py
@@ -11,6 +11,10 @@ for rodada in range(1, total_de_tentativas + 1):
     print("Você digitou ", chute_str)
     chute = int(chute_str)
 
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
+
     acertou = (numero_secreto == chute)
     maior = (chute > numero_secreto)
     menor = (chute < numero_secreto)
