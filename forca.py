8466fba55c8a3e700bd54d6464ff4a048d745a4d
Danilo Domingos <danilo.domingos02@gmail.com>
2018-11-17 12:25:31 -0200

    [#2][AULA 2][01] - Encontrando letras.

--- forca.py
+++ forca.py
@@ -5,6 +5,20 @@ def jogar():
     print("**********************************")
     print("Bem vindo ao jogo da Forca!")
     print("**********************************")
+
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual a letra: ")
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print(f"Encontrei a letra {letra} na posição {index}")
+            index += 1
     
     print("Fim do jogo.")
 
